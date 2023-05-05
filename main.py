cf4e9fc624fdf24b93b3af4563378e93022d2fbe
Sharbel <sharbelabousabha@outlook.com>
2023-05-05 21:44:49 +0300

    Backend Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import uvicorn
+from typing import List
+from pydantic import BaseModel
+from model import predict_model, train_model
+
+app = FastAPI()
+
+class MusicFeatures(BaseModel):
+    features: List[List[float]]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict")
+async def predict_genre(music_features: MusicFeatures):
+    features = music_features.features
+    prediction = predict_model(features)
+    return {"predicted_genre": prediction}
+
+@app.get("/train")
+async def train():
+    return train_model()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ model.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+import pickle
+
+def train_model():
+    
+    df = pd.read_csv('./music_data.csv', index_col = 0)
+
+    X = df.drop(['genre', 'artist', 'title', 'year','duration','dB','speachiness'], axis = 1).values
+    y = df['genre'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 21)
+
+    knn = KNeighborsClassifier()
+    knn.fit(X_train, y_train)
+
+    pickle.dump(knn, open('model.pkl', 'wb'))
+
+    return 'Model Trained with Accuracy: ' + str(knn.score(X_test, y_test))
+
+def predict_model(features):
+    knn = pickle.load(open('model.pkl', 'rb'))
+
+    # make sure features is a 2d array
+    features = np.array(features).reshape(1, -1)
+
+    return knn.predict(features)[0]
